91671b0ca8c565780bcc950eb203ff26f6c05067
Rafael Castro <rafaelcarneiroget@hotmail.com>
2018-11-06 11:16:29 -0200

    Commenting express load

--- alura/config/express.js
+++ alura/config/express.js
@@ -13,8 +13,13 @@ module.exports = () => {
     app.use(bodyParser.json());
     app.use(expressValidator());
 
+    // Aqui, 'routes' é o nome da pasta que contém o código a ser carregado. O 'cwd' otimiza a
+    // busca do express-load, falando qual é a pasta raiz de onde ele deve buscar os módulos a
+    // serem carregados. É uma configuração global, e não precisa ser refeita no 'then'. A ordem
+    // aqui é importante, precisamos carregar a rota antes, porque as rotas tem uma dependência
+    // da infra, que vem depois.
     load('routes', { cwd: 'app' })
-        // Aqui, tudo que os arquivos da pasta infra exportam se tornam objetos/funções
+        // Aqui, tudo que os arquivos da pasta 'infra' exportam se tornam objetos/funções
         // com o mesmo nome do arquivo, dentro de infra.app, como app.infra.connectionFactory.
         .then('infra')
         .into(app);
